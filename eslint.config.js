d2aaa6b241dca30599853e138340a578c0e399be
Kusstar <kussssss@outlook.com>
2024-01-05 14:23:39 +0800

    chore: eslint max-len config

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ export default antfu({
   rules: {
     'style/brace-style': ['error', '1tbs', { allowSingleLine: false }],
     'style/max-statements-per-line': 'off',
+    'style/max-len': ['error', { code: 120 }],
     'curly': ['error', 'all'],
-  }
+  },
 })
